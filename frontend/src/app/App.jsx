0dd1f14971193f3c93f7b46f401a6c372a77d254
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-07-31 18:46:55 +0300

    feature: added chat page

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "../pages/LoginPage"
-import NotFoundPage from "../pages/NotFoundPage";
+import NotFoundPage from "../pages/NotFoundPage"
+import ChatPage from '../pages/ChatPage';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<ChatPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} /> 
       </Routes>
---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,7 @@
+const ChatPage = () => {
+    return (
+        <h1>Chat Page!</h1>
+    )
+}
+
+export default ChatPage
