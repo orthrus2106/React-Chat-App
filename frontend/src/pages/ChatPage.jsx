f816fc8108a6d76e26c40e8a7b434a7f12ea7ffc
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-03 07:16:38 +0300

    fix: changes for deploy test

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import Layout from "../components/Layout"
 
 const ChatPage = () => {
     return (
-        <>
+        <Layout>
             <h1>Chat Page!</h1>
             <Link to="/login">Login</Link>
-        </>
+        </Layout>
     )
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,21 +2,42 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5002,
+//     proxy: {
+//       // Проксируем запросы к API
+//       '/api': {
+//         target: 'http://localhost:5001',
+//       },
+//       // Проксируем WebSocket соединения
+//       '/socket.io': {
+//         target: 'ws://localhost:5001',
+//         ws: true,
+//         rewriteWsOrigin: true,
+//       },
+//     },
+//   },
+// });
+const isRender = process.env.RENDER === 'true';
+
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: isRender ? undefined : {
     port: 5002,
     proxy: {
-      // Проксируем запросы к API
       '/api': {
         target: 'http://localhost:5001',
       },
-      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://localhost:5001',
         ws: true,
         rewriteWsOrigin: true,
       },
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
