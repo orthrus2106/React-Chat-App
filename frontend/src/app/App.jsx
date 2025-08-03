ac5538309a329194a018a06ec12125cecd4260a5
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-03 05:42:48 +0300

    feat: added redirection in case user unauthed

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,12 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "../pages/LoginPage"
 import NotFoundPage from "../pages/NotFoundPage"
 import ChatPage from '../pages/ChatPage';
+import AuthRequired from '../components/AuthRequired';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ChatPage />} />
+        <Route path="/" element={
+        <AuthRequired>
+          <ChatPage />
+        </AuthRequired>
+      } />
         <Route path="/login" element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} /> 
       </Routes>
---
+++ frontend/src/components/AuthRequired.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux"
+import { Navigate } from "react-router-dom"
+import { selectIsAuthed } from "../store/slices/authSlice"
+
+const AuthRequired = ({ children }) => {
+    const isAuthed = useSelector(selectIsAuthed)
+    return isAuthed ? children : <Navigate to="login" />
+}
+
+export default AuthRequired
