7222697d49cf88e18990bae0cc4bc4e9fcb63ac1
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-03 06:09:45 +0300

    feat: added logout button if user is authed

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux"
+import { selectIsAuthed } from "../store/slices/authSlice"
+import LogoutBtn from "./LogoutBtn"
+
 const Layout = ({ children }) => {
+    const isAuthed = useSelector(selectIsAuthed)
     return (
         <div className="min-vh-100 d-flex flex-column">
         <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
             <div className="container">
             <a href="/" className="navbar-brand">Hexlet Chat</a>
+            {isAuthed && <LogoutBtn />}
             </div>
         </nav>
         <main className="flex-grow-1 d-flex justify-content-center align-items-center">
---
+++ frontend/src/components/LogoutBtn.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from "react-redux"
+import { logOut } from "../store/slices/authSlice"
+import { useNavigate } from "react-router-dom"
+
+const LogoutBtn = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const handleLogOut = () => {
+        dispatch(logOut())
+        localStorage.removeItem('token')
+        navigate('/login')
+    }
+    return <button className="btn btn-primary" onClick={handleLogOut}>Выйти</button>
+}
+
+export default LogoutBtn
