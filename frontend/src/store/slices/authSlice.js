f5953e4cb988b66dc038799a70d9e5052b585993
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-04 04:43:05 +0300

    feat: added selector for token

--- frontend/src/store/slices/authSlice.js
+++ frontend/src/store/slices/authSlice.js
@@ -23,4 +23,5 @@ const authSlice = createSlice({
 
 export const { logIn, logOut } = authSlice.actions
 export const selectIsAuthed = (state) => state.auth.isAuthed
+export const selectToken = (state) => state.auth.token
 export default authSlice.reducer
