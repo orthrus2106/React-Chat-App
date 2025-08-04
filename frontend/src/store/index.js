d546ce3fe856a3844aa3273cbc0f87cfb0275645
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-04 05:50:46 +0300

    feat: added state slice for channels

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
+import channelReducer from './slices/channelSlice'
 
 export default configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        channels: channelReducer,
     }
 })
---
+++ frontend/src/store/slices/channelSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
+
+const channelsAdapter = createEntityAdapter()
+const initialState = channelsAdapter.getInitialState({
+    currentId: null,
+})
+
+const channelSlice = createSlice({
+    name: 'channels',
+    initialState,
+    reducers: {
+        addChannel: channelsAdapter.addOne,
+        removeChannel: channelsAdapter.removeOne,
+        setChannels: (state, { payload }) => {
+            channelsAdapter.setAll(state, payload)
+            state.currentId = payload[0]?.id ?? null
+        },
+        setCurrentChannel: (state, { payload }) => {
+            state.currentId = payload
+        },
+    }
+})
+
+export const { addChannel, removeChannel, setChannels, setCurrentChannel } = channelSlice.actions
+export const channelSelectors = channelsAdapter.getSelectors((state) => state.channels)
+export const selectCurrentChannelId = (state) => state.channels.currentId
+export default channelSlice.reducer
