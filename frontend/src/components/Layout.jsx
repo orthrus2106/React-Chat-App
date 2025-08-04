4512e179cedb0919d6973557b69f6b24b911a547
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-05 01:20:57 +0300

    fix: fix for main layout page

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -5,17 +5,17 @@ import LogoutBtn from "./LogoutBtn"
 const Layout = ({ children }) => {
     const isAuthed = useSelector(selectIsAuthed)
     return (
-        <div className="min-vh-100 d-flex flex-column">
-        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-            <div className="container">
-            <a href="/" className="navbar-brand">Hexlet Chat</a>
-            {isAuthed && <LogoutBtn />}
+        <div className="h-100">
+            <div className="d-flex flex-column h-100">
+                <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+                <div className="container">
+                    <a href="/" className="navbar-brand">Hexlet Chat</a>
+                    {isAuthed && <LogoutBtn />}
+                </div>
+                </nav>
+                {children}
             </div>
-        </nav>
-        <main className="flex-grow-1 d-flex justify-content-center align-items-center">
-            {children}
-        </main>
-        </div>
+            </div>
     )
 }
 
