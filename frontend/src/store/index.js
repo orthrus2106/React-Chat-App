88e0f05af9a0929185db7e97662ade3208618c92
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-06 01:44:07 +0300

    feat: ui reducer added to store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,10 +3,12 @@ import authReducer from './slices/authSlice';
 // import channelReducer from './slices/channelSlice';
 // import messageReducer from './slices/messagesSlice'
 import api from './api/apiSlice';
+import uiReducer from './slices/uiSlice'
 
 export default configureStore({
   reducer: {
     auth: authReducer,
+    ui: uiReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
