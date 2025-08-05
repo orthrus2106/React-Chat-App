d1d6125a96a70095f400eb84a793a55866d224b4
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-06 02:29:44 +0300

    feat: added component Message.jsx

---
+++ frontend/src/components/Message.jsx
@@ -0,0 +1,5 @@
+const Message = () => {
+    return <div className="text-break mb-2"><b>admin</b>: asd</div>
+}
+
+export default Message
--- frontend/src/components/MessagesBox.jsx
+++ frontend/src/components/MessagesBox.jsx
@@ -1,8 +1,9 @@
+import Message from "./Message"
+
 const MessagesBox = () => {
     return (
                             <div className="chat-messages overflow-auto px-5" id="messages-box">
-                               <div className="text-break mb-2"><b>admin</b>: asd
-                               </div>
+                                <Message />
                             </div>
     )
 }
