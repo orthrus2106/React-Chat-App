81f983490e568b92e96bdc1bfd5b76a5f815b6dd
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-10 19:37:12 +0300

    fix: ChatHeader using useActiveChannel hook

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,11 +1,16 @@
-const ChatHeader = ({ activeChannel, messages }) => {
+import useActiveChannel from "../hooks/useActiveChannel"
+import { useGetMessagesQuery } from "../store/api/apiSlice"
+
+const ChatHeader = () => {
+    const { data: messages = [] } = useGetMessagesQuery()
+    const { activeChannel } = useActiveChannel()
     return (
-                            <div className="bg-light mb-4 p-3 shadow-sm small">
-                                <p className="m-0">
-                                    <b># {activeChannel?.name}</b>
-                                </p>
-                                <span className="m-0">{messages?.filter((message) => message.channelId === activeChannel?.id).length} сообщений</span>
-                            </div>
+        <div className="bg-light mb-4 p-3 shadow-sm small">
+            <p className="m-0">
+                <b># {activeChannel?.name}</b>
+            </p>
+            <span className="m-0">{messages?.filter((message) => message.channelId === activeChannel?.id).length} сообщений</span>
+        </div>
     )
 }
 
