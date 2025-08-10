c9091b349da50dc841d1064946b00b194b99e830
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-10 19:36:07 +0300

    feat: added hook for channels

---
+++ frontend/src/hooks/useActiveChannel.js
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useGetChannelsQuery } from "../store/api/apiSlice"
+import { setCurrentChannel, selectCurrentChannelId } from "../store/slices/uiSlice"
+import { useEffect } from "react"
+
+const useActiveChannel = () => {
+    const dispatch = useDispatch()
+    const { data: channels = [], isLoading, isError } = useGetChannelsQuery()
+    const currentChannelId = useSelector(selectCurrentChannelId)
+    const activeChannel = channels.find((c) => c.id === currentChannelId) ?? null
+
+    useEffect(() => {
+        if (!isLoading && !isError && channels.length > 0 && currentChannelId === null) {
+            dispatch(setCurrentChannel(channels[0].id))
+        }
+    }, [channels, currentChannelId, dispatch, isLoading, isError])
+
+    const onSelectChannel = (id) => {
+        dispatch(setCurrentChannel(id))
+    }
+
+    return { channels, currentChannelId, activeChannel, isLoading, isError, onSelectChannel }
+}
+
+export default useActiveChannel
