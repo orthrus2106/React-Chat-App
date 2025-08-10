230c776bd5ee3bd06bb409a7fc2062b59c612a0e
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-10 18:44:52 +0300

    feat: added messages counter in chat header

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,10 +1,10 @@
-const ChatHeader = ({ activeChannel }) => {
+const ChatHeader = ({ activeChannel, messages }) => {
     return (
                             <div className="bg-light mb-4 p-3 shadow-sm small">
                                 <p className="m-0">
                                     <b># {activeChannel?.name}</b>
                                 </p>
-                                <span className="m-0">0 сообщений</span>
+                                <span className="m-0">{messages?.filter((message) => message.channelId === activeChannel?.id).length} сообщений</span>
                             </div>
     )
 }
