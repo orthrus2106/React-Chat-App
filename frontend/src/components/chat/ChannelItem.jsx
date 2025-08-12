7f808a48786739c58d4dca830cf1d1c1dc1c3f58
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-12 19:03:47 +0300

    refactor: channel item moved in self component

---
+++ frontend/src/components/chat/ChannelItem.jsx
@@ -0,0 +1,30 @@
+import { ButtonGroup, Dropdown, Button } from 'react-bootstrap';
+
+const ChannelItem = ({ channel, isActive, onSelect }) => {
+    return (
+        <Dropdown as={ButtonGroup} className='w-100'>
+            <Button
+                variant={isActive ? 'secondary' : 'light'}
+                className='w-100 rounded-0 text-start'
+                aria-current={isActive ?'true' : undefined}
+                onClick={() => onSelect(channel.id)}  
+            >
+                <span className='me-1'>#</span>
+                {channel.name}
+            </Button>
+
+            {channel.removable && (
+                <>
+                    <Dropdown.Toggle split variant={isActive ? 'secondary' : 'light'}/>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>Переименовать</Dropdown.Item>
+                         <Dropdown.Divider />
+                         <Dropdown.Item>Удалить</Dropdown.Item>
+                    </Dropdown.Menu>
+                </>
+            )}
+        </Dropdown>
+    )
+}
+
+export default ChannelItem
