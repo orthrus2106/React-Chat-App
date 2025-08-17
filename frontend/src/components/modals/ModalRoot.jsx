6b4fe08dc78a2a44adb74710e0f1d5205aabb379
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-17 22:46:31 +0300

    fix: removed console log in modal root

--- frontend/src/components/modals/ModalRoot.jsx
+++ frontend/src/components/modals/ModalRoot.jsx
@@ -8,7 +8,7 @@ const ModalRoot = () => {
     const modal = useSelector((state) => state.ui.modal)
     const dispatch = useDispatch()
     const onHide = () => dispatch(closeModal())
- console.log('ModalRoot modal =', modal);
+    
     if (modal.modalType === null) return null
 
     if (modal.modalType === 'add') {
