1aec2de49bf996bbd3d0700b8ad9301c872833a3
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-21 22:20:49 +0300

    layout: more beautiful messages

--- frontend/src/components/chat/Message.jsx
+++ frontend/src/components/chat/Message.jsx
@@ -2,4 +2,17 @@ const Message = ({ author, body }) => {
     return <div className="text-break mb-2"><b>{author}</b>: {body}</div>
 }
 
+// const Message = ({ author, body }) => {
+//   const currentUsername = localStorage.getItem('username')
+//   const isOwn = author === currentUsername
+
+//   return (
+//     <div className={`d-flex mb-2 ${isOwn ? 'justify-content-end' : 'justify-content-start'}`}>
+//       <div className={`p-2 rounded ${isOwn ? 'bg-primary text-white' : 'bg-light'}`}>
+//         <b>{author}</b>: {body}
+//       </div>
+//     </div>
+//   )
+// }
+
 export default Message
