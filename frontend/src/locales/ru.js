ad0b5511ddd7614b97c530101c609a2c324045d7
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-22 00:23:22 +0300

    feat: added russian locales

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,8 +1,60 @@
 export default {
   translation: {
-    counter_one: '{{count}} клик',
-    counter_few: '{{count}} клика',
-    counter_many: '{{count}} кликов',
-    reset_button: 'Сбросить',
+    errors: {
+      userExists: "Имя пользователя уже существует",
+      channelExists: "Имя канала уже существует",
+      addChannelFailed: "Канал не был добавлен, попробуйте позже",
+      messageSendFailed: "Сообщение не было отправлено, попробуйте позже",
+      invalidCredentials: "Неверные имя пользователя или пароль",
+      required: "Обязательное поле",
+      usernameLength: "От 3 до 20 символов",
+      minimumLength: "Минимум 3 символа",
+      maximumLength: "Максимум 20 символов",
+      passwordMin: "Не менее 6 символов",
+      passwordMismatch: "Пароли должны совпадать",
+    },
+
+    buttons: {
+      send: "Отправить",
+      remove: "Удалить",
+      cancel: "Отменить",
+      rename: "Переименовать",
+      logout: "Выйти",
+      login: "Войти",
+      register: "Регистрация",
+    },
+
+    navigation: {
+      notFound: "Страница не найдена",
+      redirect: "Но вы можете перейти",
+      mainPage: "на главную страницу",
+    },
+
+    auth: {
+      username: "Имя пользователя",
+      password: "Пароль",
+      confirmPassword: "Подтвердите пароль",
+    },
+
+    modals: {
+      addChannel: "Добавить канал",
+      enterChannelName: "Введите название канала",
+      sampleChannelName: "Например, my team",
+      removeChannel: "Удалить канал",
+      areYouSure: "Уверены?",
+      renameChannel: "Переименовать канал",
+    },
+
+    chat: {
+      newMessage: "Новое сообщение",
+      enterMessage: "Введите сообщение",
+    },
+
+    ui: {
+      channels: "Каналы",
+      messages: "сообщений",
+      appName: "Hexlet Chat",
+      noAccount: "Нет аккаунта?",
+    },
   },
-}
+};
