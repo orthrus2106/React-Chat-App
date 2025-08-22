387e12988fdd908a6f13fa01ea61f3f7cb53fb46
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-22 04:47:12 +0300

    fix: fixed locales for hexlet tests

--- frontend/src/components/page/Layout.jsx
+++ frontend/src/components/page/Layout.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { selectIsAuthed } from '../../store/slices/authSlice';
 import LogoutBtn from './LogoutBtn';
+import { Link } from 'react-router-dom';
 
 const Layout = ({ children }) => {
   const { t } = useTranslation();
@@ -11,7 +12,7 @@ const Layout = ({ children }) => {
       <div className="d-flex flex-column h-100">
         <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
           <div className="container">
-            <a href="/" className="navbar-brand">{t('ui.appName')}</a>
+            <Link to={'/'} className="navbar-brand">{t('ui.appName')}</Link>
             {isAuthed && <LogoutBtn />}
           </div>
         </nav>
