170ed9af8907b63ffe8316c298a9628b23c28d18
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-24 01:01:42 +0300

    refactor: navigation moved in component

---
+++ frontend/src/components/page/Nav.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import { useTranslation } from 'react-i18next'
+import { selectIsAuthed } from '../../store/slices/authSlice'
+import LogoutBtn from './LogoutBtn'
+import useAdaptive from "../../hooks/useAdaptive"
+
+const Nav = () => {
+    const { t } = useTranslation()
+    const isAuthed = useSelector(selectIsAuthed)
+    const { isMobile } = useAdaptive()
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container">
+            <Link to="/" className="navbar-brand">{t('ui.appName')}</Link>
+            {isAuthed && <LogoutBtn />}
+          </div>
+        </nav>
+    )
+}
+
+export default Nav
