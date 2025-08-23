9ce5f35e17cf3791b8baf89c5ee4d3d77003eb32
Eriks Lebedevs <erik.lebedevv@gmail.com>
2025-08-24 00:55:57 +0300

    feat: added hook for adaptive

---
+++ frontend/src/hooks/useAdaptive.js
@@ -0,0 +1,9 @@
+import { useMediaQuery } from 'react-responsive';
+
+const useAdaptive = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+
+  return { isMobile };
+};
+
+export default useAdaptive;
